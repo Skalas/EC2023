54e94cf4183118cec2a038e35744b569ffae157b
Miguel Escalante <miguel.escalante@kavak.com>
2023-11-01 18:42:36 -0600

    add shiny app

---
+++ notas/R/shinyApp/inicio/server.R
@@ -0,0 +1,36 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(ggplot2)
+library(nycflights13)
+library(dplyr)
+
+# Define server logic required to draw a histogram
+function(input, output, session) {
+
+    output$distPlot <- renderPlot({
+
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = paste0('Waiting time to next eruption (in mins) ',input$texto),
+             main = 'Histogram of waiting times')
+
+    })
+
+    output$tabla<- renderDataTable({
+      print(input$mes)
+      flights%>%
+        filter(month==input$mes)
+    })
+}
---
+++ notas/R/shinyApp/inicio/ui.R
@@ -0,0 +1,36 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30),
+            sliderInput("mes", "Dime un mes",min=1,max=12, value=1)
+        ),
+        
+
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotOutput("distPlot"),
+            dataTableOutput("tabla")
+        )
+    )
+)
